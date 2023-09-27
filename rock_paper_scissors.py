d28806b17df860d8b9b5fc5f4808aadab8655340
ozazza <ozvacheva@gmail.com>
2023-09-28 01:18:08 +0300

    Added more colors and question Play again

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,43 +1,51 @@
 import random
 
-from colorama import Fore
+from colorama import Fore, Back
 
 rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
 
-# Player's move
-player_move = input('Choose [r]ock, [p]aper, [s]cissors: ')
+play_again = 'y'
+while play_again == 'y':
 
-if player_move == 'r':
-    player_move = rock
-elif player_move == 'p':
-    player_move = paper
-elif player_move == 's':
-    player_move = scissors
-else:
-    raise SystemExit('Invalid input. Try again...')
+    # Player's move
+    player_move = input(Fore.LIGHTYELLOW_EX + 'Choose [r]ock, [p]aper, [s]cissors: ')
 
-# System's move
-computer_random_move = random.randint(1, 3)
-computer_move = ''
+    if player_move == 'r':
+        player_move = rock
+    elif player_move == 'p':
+        player_move = paper
+    elif player_move == 's':
+        player_move = scissors
+    else:
+        raise SystemExit('Invalid input. Try again...')
 
-if computer_random_move == 1:
-    computer_move = rock
-elif computer_random_move == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
+    # System's move
+    computer_random_move = random.randint(1, 3)
+    computer_move = ''
 
-print(f'The computer chooses {Fore.LIGHTMAGENTA_EX + computer_move}')
+    if computer_random_move == 1:
+        computer_move = rock
+    elif computer_random_move == 2:
+        computer_move = paper
+    else:
+        computer_move = scissors
 
-if player_move == computer_move or \
-        player_move == computer_move or \
-        player_move == computer_move:
-    print(Fore.BLUE + 'Draw')
-elif player_move == rock and computer_move == scissors or \
-        player_move == paper and computer_move == rock or \
-        player_move == scissors and computer_move == paper:
-    print(Fore.GREEN + 'You win')
-else:
-    print(Fore.RED + 'You lose')
+    print(Fore.YELLOW + f'The computer chooses {Fore.LIGHTMAGENTA_EX + computer_move}')
+
+    if player_move == computer_move or \
+            player_move == computer_move or \
+            player_move == computer_move:
+        print(Fore.BLUE + 'Draw')
+    elif player_move == rock and computer_move == scissors or \
+            player_move == paper and computer_move == rock or \
+            player_move == scissors and computer_move == paper:
+        print(Fore.GREEN + 'You win')
+    else:
+        print(Fore.RED + 'You lose')
+
+    play_again = input(Fore.RESET + 'Play again y/n: ')
+    if play_again == 'n':
+        print(Back.LIGHTBLACK_EX + 'Okay, see you!')
+        break
